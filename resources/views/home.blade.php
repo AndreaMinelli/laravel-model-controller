8123aabb2c0c516756f089652ea1a1ecac1f485f
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-24 16:57:30 +0100

    Creata struttura e stampati in pagina i movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,19 @@
 </head>
 
 <body>
-    <h1 class="text-center my-4">My Movies</h1>
+    @include('includes.header')
+    <main>
+        <div class="container">
+            <h1 class="text-center my-4 text-white">Movie in Room</h1>
+            <div class="row row-cols-3 gy-4">
+                @foreach ($movies as $movie)
+                    <div class="col">
+                        @include('movie.card')
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
